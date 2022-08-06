28f357ee9ee58c3e44e3a92068792a5017adec94
John8414 <jmnguyen276@gmail.com>
2022-08-06 15:43:59 +0700

    add img,booking page, scss booking page

--- src/pages/booking/booking.jsx
+++ src/pages/booking/booking.jsx
@@ -1,5 +1,243 @@
-import React from 'react'
+import React from 'react';
 
 export default function Booking() {
-    return <div>booking</div>
+    return <div class="bookingMovie ">
+        <div class="overLay">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-8 text-center">
+                        <h1 class="text-danger display-4 mb-0">BOOKING MOVIES</h1>
+                        <div class="d-flex justify-content-center">
+                            <div class=" screen mt-5"></div>
+                        </div>
+                        <div class="d-flex flex-column align-items-center text-left">
+                            <div>
+                                <div class="hangGheStyle">
+                                    <div>
+
+                                        <button class="rowNumber">1</button>
+                                        <button class="rowNumber">2</button>
+                                        <button class="rowNumber">3</button>
+                                        <button class="rowNumber">4</button>
+                                        <button class="rowNumber">5</button>
+                                        <button class="rowNumber">6</button>
+                                        <button class="rowNumber">7</button>
+                                        <button class="rowNumber">8</button>
+                                        <button class="rowNumber">9</button>
+                                        <button class="rowNumber">10</button>
+                                        <button class="rowNumber">11</button>
+                                        <button class="rowNumber">12</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div>
+                                <div class="hangGheStyle">
+                                    <div>B
+                                        <button class="ghe">B1</button>
+                                        <button class="ghe">B2</button>
+                                        <button class="ghe">B3</button>
+                                        <button class="ghe">B4</button>
+                                        <button class="ghe">B5</button>
+                                        <button class="ghe">B6</button>
+                                        <button class="ghe">B7</button>
+                                        <button class="ghe">B8</button>
+                                        <button class="ghe">B9</button>
+                                        <button class="ghe">B10</button>
+                                        <button class="ghe">B11</button>
+                                        <button class="ghe">B12</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="hangGheStyle">
+                                    <div>C
+                                        <button class="ghe">C1</button>
+                                        <button class="ghe">C2</button>
+                                        <button class="ghe">C3</button>
+                                        <button class="ghe">C4</button>
+                                        <button class="ghe">C5</button>
+                                        <button class="ghe">C6</button>
+                                        <button class="ghe">C7</button>
+                                        <button class="ghe">C8</button>
+                                        <button class="ghe">C9</button>
+                                        <button class="ghe">C10</button>
+                                        <button class="ghe">C11</button>
+                                        <button class="ghe">C12</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="hangGheStyle">
+                                    <div>D
+                                        <button class="ghe">D1</button>
+                                        <button class="ghe">D2</button>
+                                        <button class="ghe">D3</button>
+                                        <button class="ghe">D4</button>
+                                        <button class="ghe">D5</button>
+                                        <button class="ghe">D6</button>
+                                        <button class="ghe">D7</button>
+                                        <button class="ghe">D8</button>
+                                        <button class="ghe">D9</button>
+                                        <button class="ghe">D10</button>
+                                        <button class="ghe">D11</button>
+                                        <button class="ghe">D12</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="hangGheStyle">
+                                    <div>E
+                                        <button class="ghe">E1</button>
+                                        <button class="ghe">E2</button>
+                                        <button class="ghe">E3</button>
+                                        <button class="ghe">E4</button>
+                                        <button class="ghe">E5</button>
+                                        <button class="ghe">E6</button>
+                                        <button class="ghe">E7</button>
+                                        <button class="ghe">E8</button>
+                                        <button class="ghe">E9</button>
+                                        <button class="ghe">E10</button>
+                                        <button class="ghe">E11</button>
+                                        <button class="ghe">E12</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="hangGheStyle">
+                                    <div>F
+                                        <button class="ghe">F1</button>
+                                        <button class="ghe">F2</button>
+                                        <button class="ghe">F3</button>
+                                        <button class="ghe">F4</button>
+                                        <button class="ghe">F5</button>
+                                        <button class="ghe">F6</button>
+                                        <button class="ghe">F7</button>
+                                        <button class="ghe">F8</button>
+                                        <button class="ghe">F9</button>
+                                        <button class="ghe">F10</button>
+                                        <button class="ghe">F11</button>
+                                        <button class="ghe">F12</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="hangGheStyle">
+                                    <div>G
+                                        <button class="ghe">G1</button>
+                                        <button class="ghe">G2</button>
+                                        <button class="ghe">G3</button>
+                                        <button class="ghe">G4</button>
+                                        <button class="ghe">G5</button>
+                                        <button class="ghe">G6</button>
+                                        <button class="ghe">G7</button>
+                                        <button class="ghe">G8</button>
+                                        <button class="ghe">G9</button>
+                                        <button class="ghe">G10</button>
+                                        <button class="ghe">G11</button>
+                                        <button class="ghe">G12</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="hangGheStyle">
+                                    <div>H
+                                        <button class="ghe">H1</button>
+                                        <button class="ghe">H2</button>
+                                        <button class="ghe">H3</button>
+                                        <button class="ghe">H4</button>
+                                        <button class="ghe">H5</button>
+                                        <button class="ghe">H6</button>
+                                        <button class="ghe">H7</button>
+                                        <button class="ghe">H8</button>
+                                        <button class="ghe">H9</button>
+                                        <button class="ghe">H10</button>
+                                        <button class="ghe">H11</button>
+                                        <button class="ghe">H12</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="hangGheStyle">
+                                    <div>I
+                                        <button class="ghe">I1</button>
+                                        <button class="ghe">I2</button>
+                                        <button class="ghe">I3</button>
+                                        <button class="ghe">I4</button>
+                                        <button class="ghe">I5</button>
+                                        <button class="ghe">I6</button>
+                                        <button class="ghe">I7</button>
+                                        <button class="ghe">I8</button>
+                                        <button class="ghe">I9</button>
+                                        <button class="ghe">I10</button>
+                                        <button class="ghe">I11</button>
+                                        <button class="ghe">I12</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="hangGheStyle">
+                                    <div>J
+                                        <button class="ghe">J1</button>
+                                        <button class="ghe">J2</button>
+                                        <button class="ghe">J3</button>
+                                        <button class="ghe">J4</button>
+                                        <button class="ghe">J5</button>
+                                        <button class="ghe">J6</button>
+                                        <button class="ghe">J7</button>
+                                        <button class="ghe">J8</button>
+                                        <button class="ghe">J9</button>
+                                        <button class="ghe">J10</button>
+                                        <button class="ghe">J11</button>
+                                        <button class="ghe">J12</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4 text-center">
+                        <h1 class="text-danger display-4">ORDER LIST</h1>
+                        <div>
+                            <div class="d-flex flex-column">
+                                <div class="noteSpan">
+                                    <button class="note__booked"></button><span class="text-white pl-2">Booked
+                                        Seat</span>
+                                </div>
+                                <div class="noteSpan">
+                                    <button class="note__selecting"></button><span class="text-white pl-2">Selecting
+                                        Seat</span>
+                                </div>
+                                <div class="noteSpan">
+                                    <button class="note__ghe">
+
+                                    </button>
+                                    <span class="text-white pl-2">
+                                        Available Seats
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="mt-5">
+                                <table class="table text-white" border="2">
+                                    <thead>
+                                        <tr>
+                                            <th class="text-light">Seat No.</th>
+                                            <th class="text-light">Price</th>
+                                            <th class="text-light">Cancel</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody></tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <td>Totals</td>
+                                            <td>0</td>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>;
 }
