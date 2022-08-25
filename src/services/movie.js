c927f9cd5b7b2f32c5fe562a775ad12990ac9b82
hongthanh711 <phthanh1999vvk@gmail.com>
2022-08-25 18:15:51 +0700

    update delete movie

--- src/services/movie.js
+++ src/services/movie.js
@@ -1,39 +1,47 @@
-import { GROUP_ID } from 'constants/common';
-import { request } from '../configs/axios';
+import { GROUP_ID } from 'constants/common'
+import { request } from '../configs/axios'
 
 const fetchMovieListApi = () => {
-  return request({
-    url: `/QuanLyPhim/LayDanhSachPhim?maNhom=${GROUP_ID}`,
-    method: 'GET',
-  });
-};
+    return request({
+        url: `/QuanLyPhim/LayDanhSachPhim?maNhom=${GROUP_ID}`,
+        method: 'GET',
+    })
+}
 
 const fetchMovieDetailApi = (movieId) => {
-  return request({
-    url: `/QuanLyPhim/LayThongTinPhim?MaPhim=${movieId}`,
-    method: 'GET',
-  });
-};
+    return request({
+        url: `/QuanLyPhim/LayThongTinPhim?MaPhim=${movieId}`,
+        method: 'GET',
+    })
+}
 
 const addMovieUploadImage = (data) => {
-  return request({
-    url: '/QuanLyPhim/ThemPhimUploadHinh',
-    method: 'POST',
-    data,
-  });
-};
+    return request({
+        url: '/QuanLyPhim/ThemPhimUploadHinh',
+        method: 'POST',
+        data,
+    })
+}
 
 const updateMovieUploadImage = (data) => {
-  return request({
-    url: '/QuanLyPhim/CapNhatPhimUpload',
-    method: 'POST',
-    data,
-  });
-};
+    return request({
+        url: '/QuanLyPhim/CapNhatPhimUpload',
+        method: 'POST',
+        data,
+    })
+}
+
+const deleteMovieApi = (movieId) => {
+    return request({
+        url: `/QuanLyPhim/XoaPhim?MaPhim=${movieId}`,
+        method: 'DELETE',
+    })
+}
 
 export {
-  updateMovieUploadImage,
-  fetchMovieListApi,
-  fetchMovieDetailApi,
-  addMovieUploadImage,
-};
+    updateMovieUploadImage,
+    fetchMovieListApi,
+    fetchMovieDetailApi,
+    addMovieUploadImage,
+    deleteMovieApi,
+}
