093ba0aedd758a2219995ed3e984de29a2947aa4
John8414 <jmnguyen276@gmail.com>
2022-09-01 18:50:03 +0700

    update UI by Nguyen

---
+++ src/components/icon/icon-bhd.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import iconBhd from '../../assets/images/icon-bhd.png';
+import Icon from './icon';
+
+export const IconBhd = () => {
+    return (
+        <Icon src={iconBhd} />
+    );
+};
+
+export default IconBhd;
---
+++ src/components/icon/icon-logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import iconLogo from '../../assets/images/icon-logo.png';
+import Icon from './icon';
+
+export const IconLogo = () => {
+    return (
+        <Icon src={iconLogo} />
+    );
+};
+
+export default IconLogo;
